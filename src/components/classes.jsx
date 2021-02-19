69aed4c29b48333825cfe03e6c3c997e33545595
GeoffReid <geoff_reid@outlook.com>
2021-02-19 16:33:19 +1100

    Added table css and class read functions

--- src/components/classes.jsx
+++ src/components/classes.jsx
@@ -1,9 +1,54 @@
 import React, { Component } from "react";
+import { getClasses } from "../services/classService";
+
+// CSS
+import styles from "./css/styles.module.css";
+import headingStyles from "./css/headingStyles.module.css";
 
 class Classes extends Component {
-	state = {};
+	state = {
+		classes: getClasses(),
+	};
+
+	handleOpenClassList = (classItem) => {
+		// Creates new movies array, excluding particular movie ID
+		//const newMovies = this.state.movies.filter((m) => m._id !== movie._id);
+		console.log(classItem._id);
+	};
+
 	render() {
-		return <h1>Hi</h1>;
+		return (
+			<React.Fragment>
+				<h1 className={headingStyles.pageTitle}>Class Enrolments</h1>
+				<table className={styles.GreyTable}>
+					<thead className={styles.GreyTableHeader}>
+						<tr>
+							<th className={styles.GreyTableText}>Code</th>
+							<th className={styles.GreyTableText}>Name</th>
+							<th className={styles.GreyTableText}>Capacity</th>
+							<th className={styles.GreyTableText}>Class List</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.classes.map((classItem) => (
+							<tr className={styles.GreyTableRow} key={classItem._id}>
+								<td className={styles.GreyTableText}>{classItem.code}</td>
+								<td className={styles.GreyTableText}>{classItem.name}</td>
+								<td className={styles.GreyTableText}></td>
+								<td className={styles.GreyTableButtonTD}>
+									<button
+										className="btn btn-info btn-sm"
+										onClick={() => this.handleOpenClassList(classItem)}
+									>
+										Open
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</React.Fragment>
+		);
 	}
 }
 
